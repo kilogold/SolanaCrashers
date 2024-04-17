bf630cdfe07a2df4413116883a17391124fed805
kilogold <bonilla_kelvin@hotmail.com>
2024-04-16 21:22:41 -0700

    Abstracting away account complexity.

--- Assets/SolanaCrashers/FungiblesListener.cs
+++ Assets/SolanaCrashers/FungiblesListener.cs
@@ -1,26 +1,15 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Cysharp.Threading.Tasks;
-using Cysharp.Threading.Tasks.Linq;
-using Solana.Unity.Programs;
-using Solana.Unity.Rpc.Core.Http;
 using Solana.Unity.Rpc.Core.Sockets;
 using Solana.Unity.Rpc.Types;
 using Solana.Unity.SDK;
 using Solana.Unity.Wallet;
 using SolCrashersOnChain;
-using SolCrashersOnChain.Program;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace UIToolkitDemo
 {
     public class FungiblesListener : MonoBehaviour
     {
-        private const string goldATA = "6d8tL1FJPcJ8mn4wekbiQQzSV2rMGYSY7ab3xHMR4Kt9";
-        private const string gemsATA = "DXqGMepER7SeJmsfPgkb7eoewQQZXfbgHGB7ka9ZHJy1";
         public struct Fungibles
         {
             public uint gameCurrency;
@@ -38,6 +27,9 @@ namespace UIToolkitDemo
         
         async void Awake()
         {
+            SolCrashersOnChainClient.DeriveMintAndTokenAccountsFor("gold", out _, out PublicKey goldATA);
+            SolCrashersOnChainClient.DeriveMintAndTokenAccountsFor("gems", out _, out PublicKey gemsATA);
+
             subscriptionGold = await Web3.WsRpc.SubscribeTokenAccountAsync(goldATA, (state, value) =>
             {
                 subscriptionCache.gameCurrency = (uint)value.Value.Data.Parsed.Info.TokenAmount.AmountUlong;
@@ -52,6 +44,7 @@ namespace UIToolkitDemo
                 TriggerFungiblesUpdate(subscriptionCache);
             }, Commitment.Confirmed);
 
+            // Refresh currency when game data gets loaded.
             SaveManager.GameDataLoaded += async _ =>
             {
                 var output = await Web3.Rpc.GetTokenAccountBalanceAsync(goldATA);
@@ -92,73 +85,14 @@ namespace UIToolkitDemo
         {
             if (Input.GetKeyDown(KeyCode.P))
             {
-                var client = new  SolCrashersOnChain.SolCrashersOnChainClient(
-                    Web3.Rpc,
-                    Web3.WsRpc,
-                    new PublicKey("4LaDUjM73BdtSxjAH15rJmGv4aLYGHSMRtXDK6BWhhyq")
-                );
-
-                var accts = new PrintGoldAccounts
-                {
-                    DstAta = new PublicKey("6d8tL1FJPcJ8mn4wekbiQQzSV2rMGYSY7ab3xHMR4Kt9"),
-                    Payer = Web3.Wallet.Account.PublicKey,
-                    Mint = new PublicKey("7syYaUinTxXedKUQEBU8yVeR8ffhpCJkbDYraPbpardL"),
-                    AssociatedTokenProgram = AssociatedTokenAccountProgram.ProgramIdKey,
-                    SystemProgram = SystemProgram.ProgramIdKey,
-                    TokenProgram = TokenProgram.ProgramIdKey,
-                    Rent = SysVars.RentKey
-                };
-                
-                SendPrintGoldAsync(client, accts).Forget();
+                SolCrashersOnChainClient.PrintGold(20);
             }
+
             if (Input.GetKeyDown(KeyCode.L))
             {
-                var client = new  SolCrashersOnChain.SolCrashersOnChainClient(
-                    Web3.Rpc,
-                    Web3.WsRpc,
-                    new PublicKey("4LaDUjM73BdtSxjAH15rJmGv4aLYGHSMRtXDK6BWhhyq")
-                );
+                SolCrashersOnChainClient.PrintGems(1);
 
-                var accts = new PrintGemsAccounts()
-                {
-                    DstAta = new PublicKey("DXqGMepER7SeJmsfPgkb7eoewQQZXfbgHGB7ka9ZHJy1"),
-                    Payer = Web3.Wallet.Account.PublicKey,
-                    Mint = new PublicKey("4krPZJTGHFX1UpYsqz6ndE5vEo54E9qiyTG9ENwqRvmz"),
-                    SystemProgram = SystemProgram.ProgramIdKey,
-                    TokenProgram = TokenProgram.ProgramIdKey,
-                    Rent = SysVars.RentKey
-                };
-                
-                SendPrintGemsAsync(client, accts).Forget();
             }
-        }
-
-        private async UniTask SendPrintGoldAsync(SolCrashersOnChainClient client, PrintGoldAccounts accts)
-        {
-            var result = await client.SendPrintGoldAsync(
-                accts,
-                45,
-                Web3.Wallet.Account.PublicKey,
-                (bytes, key) => Web3.Wallet.Account.Sign(bytes),
-                client.ProgramIdKey
-            );
-            
-            Debug.Log(result.Result);
-            Debug.Log(result.Reason);
-        }
-        
-        private async UniTask SendPrintGemsAsync(SolCrashersOnChainClient client, PrintGemsAccounts accts)
-        {
-            var result = await client.SendPrintGemsAsync(
-                accts,
-                12,
-                Web3.Wallet.Account.PublicKey,
-                (bytes, key) => Web3.Wallet.Account.Sign(bytes),
-                client.ProgramIdKey
-            );
-            
-            Debug.Log(result.Result);
-            Debug.Log(result.Reason);
         }
     }
 }
